b21b082e4eaf6c1ae9561a485a0500db2069a6a4
xv-erico-djasmeini <108249559+xv-erico-djasmeini@users.noreply.github.com>
2023-06-21 14:45:35 +0800

    U

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
     if not fruit_choice:
         streamlit.write('The user entered ', fruit_choice)
-    else
+    else:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
         # write your own comment -what does the next line do? 
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
