e5254c79b48c96cbd9df6d869be15378144759f0
xv-erico-djasmeini <108249559+xv-erico-djasmeini@users.noreply.github.com>
2023-06-21 12:09:54 +0800

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,4 +32,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframes(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
